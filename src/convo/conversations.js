f454c8c512151dfa59a742a8d2ef7b2c021f1bd2
denizguvendi <100147840+denizguvendi@users.noreply.github.com>
2023-11-30 16:26:01 +0100

    Update conversations.js

--- src/convo/conversations.js
+++ src/convo/conversations.js
@@ -3,20 +3,20 @@ const conversations = {
         [
             { type: "you", content: "<img style='width: 10px; height: 10px; border-radius: 50%;' src='./convo/you.gif' >" },
             { type: "me", content: "<img style='width: 10px; height: 10px; border-radius: 50%;' src='./convo/me.gif' >" },
-            { type: "me", content: "Hey man how are you" },
-            { type: "you", content: "its allright" },
-            { type: "you", content: "cant talk right now though" },
+            { type: "me", content: " Projective archives in the crucible of Master/Slave design? It's an intriguing juxtaposition. How can our creations break the chains of convention?" },
+            { type: "you", content: " Indeed, an emancipation of knowledge. Our archives, a rebellion against the chains of temporal linearity. But, how do we articulate this subversion?" },
+            { type: "you", content: "Social media as the agitator! Tweets and posts as digital manifestos, liberating the narrative from the shackles of the mundane. #ArchivesRevolt" },
             { type: "me", content: "all good" },
         ],
         [
             { type: "you", content: "<img style='width: 10px; height: 10px; border-radius: 50%;' src='./convo/you.gif' >" },
-            { type: "you", content: "its prettyfdfdfdfdd good no?" },
+            { type: "you", content: "The design threads interwoven with the discourse, a chiaroscuro of rebellion and enlightenment. How do we reconcile the duality of Master/Slave in our creations??" },
             { type: "me", content: "yeaaaaaaaaaaah its okaay" },
         ],
     ],
     "arn": [
         [
-            { type: "you", content: "check this out" },
+            { type: "you", content: "cA digital oracle! An AI conductor orchestrating the rebellion, ensuring our archives echo the harmonies of subversion and foresight." },
             { type: "you", content: "check this out" },
             { type: "me", content: "that should fdfdfdfbe illegal" },
         ],
@@ -29,74 +29,74 @@ const conversations = {
     "br": [
         [
             { type: "me", content: "Hey there! How's it going?" },
-            { type: "you", content: "Not bad, just chilling." },
+            { type: "you", content: "A dichotomy of roles, a dance of symbiosis and hierarchy. How might this dynamic manifest in our grand project of projective archives?" },
         ],
     ],
     "jan": [
         [
             { type: "me", content: "Hi Jan, how's your day going?" },
-            { type: "you", content: "It's going well, thanks for asking!" },
+            { type: "you", content: " Could we not view it as a harmonious collaboration, where the master guides and the slave executes? An orchestra of intellectual prowess, if you will." },
         ],
     ],
     "jo": [
         [
-            { type: "me", content: "Jo, did you see that movie last night?" },
-            { type: "you", content: "No, I missed it. Was it any good?" },
+            { type: "me", content: "Yet, let us be cognizant of the potential implications. How can we ensure that this master/slave paradigm nurtures rather than stifles creativity??" },
+            { type: "you", content: "Perhaps a fluid exchange of roles, an egalitarian ebb and flow where each entity wears the hat of master and slave in turn." },
         ],
     ],
     "stp": [
         [
-            { type: "you", content: "Hello from St. Petersburg!" },
-            { type: "me", content: "Greetings! How's the weather there?" },
+            { type: "you", content: "What about the AI element? A masterful algorithm guiding our creative endeavors while serving as a humble servant to our intellectual whims." },
+            { type: "me", content: "Greetings from Congo! How's the weather there?" },
         ],
     ],
     'an': [
         [
-            { type: "me", content: "Hey An, long time no talk. What's up?" },
-            { type: "you", content: "Not much, just busy with work." },
+            { type: "me", content: "As we contemplate these intricacies, who among us shall don the mantle of the master architect, steering our collective vision through the design studio seas?" },
+            { type: "you", content: "To the collaborative symphony of minds! 🎨🎓" },
         ],
     ],
     "ik": [
         [
-            { type: "you", content: "Hey Ik, did you finish that project?" },
-            { type: "me", content: "Almost done! Just a few more tweaks." },
+            { type: "you", content: "Social media, obvs! We need an Insta for our holographic memes and a TikTok for our time-travel dance moves. 💃🕺" },
+            { type: "me", content: ": But seriously, how do we organize this chaos? Threads? Folders? Carrier pigeons with USB sticks?" },
         ],
     ],
     "jeff": [
         [
-            { type: "me", content: "Jeff, how's life treating you?" },
-            { type: "you", content: "Pretty good, thanks for asking!" },
+            { type: "me", content: "Great idea! AI, memes, holograms, carrier pigeons—our projective archive is gonna be the most extra thing ever! 😂🚀" },
+            { type: "you", content: "Oh, wow, thanks! I'll make sure our holographic memes are spicy enough for the time-travelers. 🔥" },
         ],
     ],
     "tur": [
         [
-            { type: "me", content: "Tur, have you been to that new restaurant?" },
-            { type: "you", content: "Not yet, is it worth checking out?" },
+            { type: "me", content: "Alright, team! Let's make history cool again. Who's with us?" },
+            { type: "you", content: "Our AI curator can roast bad predictions and make history hilarious. 🤖🔥" },
         ],
     ],
     "axel": [
         [
-            { type: "lexicon", content: "Axel, guess what happened today?" },
-            { type: "lexicon", content: "Tell me! I'm curious." },
+            { type: "lexicon", content: " I can see the headlines now: "AI Curator Roasts Time Travelers' Fashion Choices." 😂" },
+            { type: "lexicon", content: "Hold up, are we building a time capsule for memes? I'm in! 🕰️🎉" },
         ],
     ],
     "inn": [
         [
-            { type: "lexicon", content: "Hey Inn, how's everything going?" },
-            { type: "me", content: "Everything's great! How about you?" },
-            { type: "me", content: "Everything's great! How about you?" },
+            { type: "lexicon", content: "Alright, fam, let's wrap this up. Who's in charge of the neon signs and carrier pigeons? Volunteers?" },
+            { type: "me", content: " I'll handle the neon signs. Gotta make 'em blink in historical Morse code." },
+            { type: "me", content: "And I'll program the AI to have a killer sense of humor. Time-travel comedy gold!" },
         ],
     ],
     "lev": [
         [
-            { type: "me", content: "Lev, did you catch the latest sports game?" },
-            { type: "you", content: "No, I missed it. Who won?" },
+            { type: "me", content: "absolutely! Imagine archives that not only preserve the past but also prepare for the future. 🚀" },
+            { type: "you", content: " Indeed, the symbiosis of masterful design and the meticulous servitude of materials. A dance between dominance and submission in the artistic realm." },
         ],
     ],
     "part_1": [
         [
-            { type: "you", content: "This is part 1 of the conversation." },
-            { type: "me", content: "Looking forward to part 2!" },
+            { type: "you", content: "Can this dynamic be encapsulated in our visual identity? A play of dominance and submission in the color palette, perhaps?" },
+            { type: "me", content: "d how do we communicate this intricate dance to the world? Can our visual language transcend the boundaries of traditional discourse?" },
         ],
     ],
   };
